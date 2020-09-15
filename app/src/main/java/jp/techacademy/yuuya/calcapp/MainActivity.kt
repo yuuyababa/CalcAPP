ba810b644ebbf8aac5988f81be9e360eff734612
yuuyababa <aloneperversity@ezweb.ne.jp>
2020-09-15 23:28:28 +0900

    課題提出用コミット

--- app/src/main/java/jp/techacademy/yuuya/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuuya/calcapp/MainActivity.kt
@@ -6,6 +6,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import android.view.View
 import androidx.annotation.IntegerRes
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity(),View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,20 +21,48 @@ class MainActivity : AppCompatActivity(),View.OnClickListener{
     }
 
     override fun onClick(p0: View) {
-        val calValue1 = Integer.parseInt(value1.getText().toString())
-        val calValue2 = Integer.parseInt(value2.getText().toString())
+        val setValue1 = value1.getText().toString()
+        val setValue2 = value2.getText().toString()
 
-    when(p0.id){
-        R.id.Pulus      ->changePage()
-        R.id.Minus      ->changePage()
-        R.id.Multiplied ->changePage()
-        R.id.Divided    ->changePage()
-                }
+        if (setValue1 != "" && setValue2 != "") {
+
+
+
+            val calcValue1: Double = setValue1.toDouble()
+            val calcValue2: Double = setValue2.toDouble()
+
+            when (p0.id) {
+                R.id.Pulus -> changePage(calcValue1, calcValue2, "Pu")
+                R.id.Minus -> changePage(calcValue1, calcValue2, "Mi")
+                R.id.Multiplied -> changePage(calcValue1, calcValue2, "Mu")
+                R.id.Divided -> changePage(calcValue1, calcValue2, "Di")
+            }
+        }
+
+        val snackbar = Snackbar.make(p0,"値を入力して下さい",Snackbar.LENGTH_SHORT)
+        snackbar.show()
+
+        return
     }
 
 
-    private fun changePage( ) {
-        val intent = Intent(this, result::class.java)
-        startActivity(intent)
-    }
+    private fun changePage( calvalue1:Double,calvalue2:Double, type:String) {
+        val Result: Double
+
+
+            if (type == "Pu") {
+                Result = calvalue1 + calvalue2
+            } else if (type == "Mi") {
+                Result = calvalue1 - calvalue2
+            } else if (type == "Mu") {
+                Result = calvalue1 * calvalue2
+            } else {
+                Result = calvalue1 / calvalue2
+            }
+
+            val intent = Intent(this, result::class.java)
+            intent.putExtra("Result", Result)
+            startActivity(intent)
+        }
+
 }
--- app/src/main/java/jp/techacademy/yuuya/calcapp/result.kt
+++ app/src/main/java/jp/techacademy/yuuya/calcapp/result.kt
@@ -2,10 +2,15 @@ package jp.techacademy.yuuya.calcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class result : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val viewResult : Double = intent.getDoubleExtra("Result", 0.0)
+
+        result.text = "${viewResult}"
     }
 }
